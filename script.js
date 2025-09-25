a9a2cf3aac112e81316671840c1f2be81ed7b1d5
unknown <anushreemaha123@gmail.com>
2025-09-25 13:31:52 +0530

    JavaScript project

---
+++ script.js
@@ -0,0 +1,32 @@
+// first buy
+function up(){
+    document.getElementById('img').style.backgroundImage="url('./assets/saree-2.jpg')"
+}
+function down(){
+    document.getElementById('img').style.backgroundImage="url('./assets/saree-1.jpg')"
+}
+
+
+// second buy
+function up1(){
+    document.getElementById('img').style.backgroundImage="url('./assets/2nd-saree option.jpg')"
+}
+function down1(){
+    document.getElementById('img').style.backgroundImage="url('./assets/2nd-saree-option2.jpg')"
+}
+
+//Third buy
+function up2(){
+    document.getElementById('img').style.backgroundImage="url('./assets/3rd-saree-option.jpg')"
+}
+function down2(){
+    document.getElementById('img').style.backgroundImage="url('./assets/3rd-saree-option2.jpg')"
+}
+
+//Fouth buy
+function up3(){
+    document.getElementById('img').style.backgroundImage="url('./assets/4th-saree-option.jpg')"
+}
+function down3(){
+    document.getElementById('img').style.backgroundImage="url('./assets/4th-saree-option1.jpg')"
+}
